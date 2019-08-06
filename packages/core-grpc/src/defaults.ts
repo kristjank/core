fd06e2fb47a3e727e4103ab0d12d28fb7c2768ef
Kristjan Kosic - Chris <chris@ark.io>
2019-08-06 14:12:59 +0200

    chore: working blocks stream sample, rem greeter1

--- packages/core-grpc/src/defaults.ts
+++ packages/core-grpc/src/defaults.ts
@@ -2,15 +2,9 @@ export const defaults = {
     protoFolder: "/Users/chris/_WORK/ARK/core/packages/core-grpc/src/protos/",
     services: [
         {
-            enabled: true,
+            enabled: false,
             className: "Greeter",
             package: "greeter",
-            protoFileName: "service.proto",
-        },
-        {
-            enabled: true,
-            className: "Greeter1",
-            package: "greeter1",
             protoFileName: "service.proto",
         },
         {
--- packages/core-grpc/src/services/blocks.ts
+++ packages/core-grpc/src/services/blocks.ts
@@ -1,14 +1,40 @@
+import { ApplicationEvents } from "@arkecosystem/core-event-emitter";
 import { GrpcObject } from "grpc";
-import { sendUnaryData, ServerUnaryCall } from "grpc";
+import { sendUnaryData, ServerUnaryCall, ServerWriteableStream } from "grpc";
 import { BaseService } from "./base-service";
 
 export class Blocks extends BaseService {
-    public async getBlocks(call: ServerUnaryCall<GrpcObject>, callback: sendUnaryData<GrpcObject>) {
+    public async getBlocks(call: ServerUnaryCall<GrpcObject>, callback: sendUnaryData<GrpcObject>): Promise<void> {
         this.logger.info(`RPC Request from ${call.getPeer()} request: ${call.request.id}$`);
         const block = await this.blocksRepository.findById(call.request.id);
         this.logger.info(block);
 
         // tslint:disable-next-line: no-null-keyword
         callback(null, { id: block.id, reward: block.reward });
+    }
+
+    public getForgedBlocksAsStream(call: ServerWriteableStream<GrpcObject>): void {
+        let i: number = 10;
+
+        this.emitter.on(ApplicationEvents.BlockForged, () => {
+            const block = this.blockchain.getLastBlock().data;
+            this.logger.info(block);
+            call.write({
+                id: block.id,
+                version: block.version,
+                timestamp: block.timestamp,
+                height: block.height,
+                reward: block.reward,
+                previousBlock: block.previousBlock,
+                totalAmount: block.totalAmount,
+                generatorPublicKey: block.generatorPublicKey,
+            });
+
+            i--;
+
+            if (i < 0) {
+                call.end();
+            }
+        });
     }
 }
